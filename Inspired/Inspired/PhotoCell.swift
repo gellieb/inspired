cf4c970d0f1b44ab874f9a03feec08d1fa54267e
Angel Baek <angelbaek1@gmail.com>
2015-06-24 17:19:12 -0400

    create photocell class and photofeed controller

---
+++ Inspired/Inspired/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  Inspired
+//
+//  Created by Angel Baek on 6/24/15.
+//  Copyright (c) 2015 Angel Baek. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
